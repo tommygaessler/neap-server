adef2f35e62ac7df283a9963c343ff601b2d7e26
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-10-24 10:59:28 -0600

    deploying

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,16 @@ module.exports = {
       directory: __dirname + '/src/server/db/seeds'
     }
   },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  },
   test: {
     client: 'postgresql',
     connection: `postgres://localhost:5432/${databaseName}_test`,
